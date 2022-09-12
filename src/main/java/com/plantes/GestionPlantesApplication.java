dd8fc45e3b649d168a60e1ae770d4499d0cbcc6f
Raphaël Farina <raphfar@gmail.com>
2022-09-12 16:16:37 +0200

    init project

---
+++ src/main/java/com/plantes/GestionPlantesApplication.java
@@ -0,0 +1,13 @@
+package com.plantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPlantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPlantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantes/GestionPlantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPlantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
